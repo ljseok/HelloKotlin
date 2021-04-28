0331473118db504277fbefbfc053647837b4fbc0
ccccljs <cho38suk@hamail.net>
2021-04-28 15:43:25 +0900

    Elapsed Seconds

--- app/src/main/java/com/example/hellokotlin/VariablejavaActivity.java
+++ app/src/main/java/com/example/hellokotlin/VariablejavaActivity.java
@@ -11,10 +11,11 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class VariablejavaActivity extends AppCompatActivity {
-    TextView txtActivityStartTime , txtCountbtnCliks;
+    TextView txtActivityStartTime , txtCountbtnCliks , txtElapsedTime;
     Button btnClickme;
 
     final long startTime = System.currentTimeMillis();
+    final long elapsedSecond =(( System.currentTimeMillis()-startTime)/1000);
 
     int clickcount = 0;
 
@@ -27,6 +28,7 @@ public class VariablejavaActivity extends AppCompatActivity {
 
         txtActivityStartTime = findViewById(R.id.txt_activityStarttime);
         txtCountbtnCliks = findViewById(R.id.txt_countbtnClicks);
+        txtElapsedTime = findViewById(R.id.txtElaspedTime);
         btnClickme = findViewById(R.id.btnClickme);
 
         btnClickme.setOnClickListener(new View.OnClickListener() {
@@ -39,5 +41,7 @@ public class VariablejavaActivity extends AppCompatActivity {
         String timetext = new SimpleDateFormat("HH:mm:ss", Locale.KOREA).format(startTime);
         txtActivityStartTime.setText("Activity start time = " + timetext);
         txtCountbtnCliks.setText("Ready to start counting cliks ");
+        String elapesdTime = new SimpleDateFormat("ss Seconds delay",Locale.KOREA).format(elapsedSecond);
+        txtElapsedTime.setText("Elapsed time =" + elapesdTime);
     }
 }
--- app/src/main/java/com/example/hellokotlin/VariablekotlinActivity.kt
+++ app/src/main/java/com/example/hellokotlin/VariablekotlinActivity.kt
@@ -10,6 +10,7 @@ import java.util.*
 class VariablekotlinActivity : AppCompatActivity() {
     var clickcount = 0;
     var startTime = System.currentTimeMillis();
+    var elapsed   = ((System.currentTimeMillis()-startTime)/1000.0).toLong()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +18,7 @@ class VariablekotlinActivity : AppCompatActivity() {
 
         val txtActivityStartTime = findViewById<TextView>(R.id.txt_activityStarttime)
         val txtCountBtnCliks = findViewById<TextView>(R.id.txt_countbtnClicks)
-
+        val txtElapsedTime = findViewById<TextView>(R.id.txtElaspedTime)
         val btnClickMe = findViewById<Button>(R.id.btnClickme)
 
         btnClickMe.setOnClickListener {
@@ -26,5 +27,8 @@ class VariablekotlinActivity : AppCompatActivity() {
         }
         val timedText = SimpleDateFormat("HH:mm:ss", Locale.KOREAN).format(startTime);
         txtActivityStartTime.text = "Activity Start Time = +$(timeText)";
+        val ElapsedText = SimpleDateFormat("ss Seconds delay", Locale.KOREAN).format(elapsed);
+        txtElapsedTime.text = "Elapsed Time = +$(ElapsedText)";
+
     }
 }
